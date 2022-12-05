3c42fd2af40b980f9193849b1b0f89849cf3a10f
AvanMyers <smail1341@gmail.com>
2022-12-05 14:23:06 +0300

    done

---
+++ WorkingWithTableRows/WorkingWithTableRows/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithTableRows
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int summSecondLine = 0;
+            int productOfNumberFirstColumn = 1;
+            int[,] array = {
+                { 3, 4, 5, 9 },
+                { 5, 1, 1, 5 },
+                { 5, 8, 4, 21 } };
+
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (i == 1)
+                    {
+                        summSecondLine+= array[i, j];
+                    }
+
+                    if (j == 0)
+                    {
+                        productOfNumberFirstColumn *= array[i, 0];
+                    }
+                }
+            }
+            Console.WriteLine($"Сумма 2-й строки составляет - {summSecondLine}, произведение первого столбца составляет - {productOfNumberFirstColumn}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WorkingWithTableRows/WorkingWithTableRows/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("WorkingWithTableRows")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WorkingWithTableRows")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c46597ff-09e6-4b66-bf5f-f34eba726c82")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
