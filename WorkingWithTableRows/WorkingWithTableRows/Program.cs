27956e18d06028782ce7ca5645cd86d567695e4c
AvanMyers <smail1341@gmail.com>
2022-12-07 12:33:52 +0300

    rebild

--- WorkingWithTableRows/WorkingWithTableRows/Program.cs
+++ WorkingWithTableRows/WorkingWithTableRows/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,32 +10,54 @@ namespace WorkingWithTableRows
     internal class Program
     {
         static void Main(string[] args)
-        {
-            int summSecondLine = 0;
-            int productOfNumberFirstColumn = 1;
-            int[,] array = {
-                { 3, 4, 5, 9 },
-                { 5, 1, 1, 5 },
-                { 5, 8, 4, 21 } };
+        { 
+            Random random= new Random();
+            int arraySize = 3;
+            int arrayDeep = 5;
+            int minRandom = 1;
+            int maxRandom = 10;
+            int lineForSumm = 1;
+            int rowForProduct = 0;
+            int summLine = 0;
+            int productOfNumberColumn = 1;
+            int[,] array = new int[arraySize, arrayDeep];
 
+            for (int i = 0; i < arraySize; i++)
+            {
+                Console.Write("[ ");
+
+                for (int j = 0; j < arrayDeep; j++)
+                {
+                    array[i, j] = random.Next(minRandom, maxRandom);
+                    Console.Write(array[i,j] + " ");
+                }
+
+                Console.WriteLine(" ]");
+            }
 
             for (int i = 0; i < array.GetLength(0); i++)
             {
-
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
-                    if (i == 1)
+                    if (i == lineForSumm)
                     {
-                        summSecondLine+= array[i, j];
-                    }
-
-                    if (j == 0)
-                    {
-                        productOfNumberFirstColumn *= array[i, 0];
+                        summLine += array[i, j];
                     }
                 }
             }
-            Console.WriteLine($"Сумма 2-й строки составляет - {summSecondLine}, произведение первого столбца составляет - {productOfNumberFirstColumn}");
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (j == rowForProduct)
+                    {
+                        productOfNumberColumn *= array[i, rowForProduct]; 
+                    }
+                }
+            }
+
+            Console.WriteLine($"Сумма 2-й строки составляет - {summLine}, произведение первого столбца составляет - {productOfNumberColumn}");
             Console.ReadKey();
         }
     }
